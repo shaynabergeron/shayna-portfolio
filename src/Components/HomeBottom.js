45e46d5023c9b010cdeb481a86f6493c6550e865
Shayna Bergeron <shayna.celine@gmail.com>
2022-10-23 00:38:36 -0500

    V 1.1.0 Started homepage components

---
+++ src/Components/HomeBottom.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import { AiFillGuthub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+/** Style Sheet Import */
+import "./HomeButton.css";
+
+/** TODO: Finish rest of component, for main homepage component
+ * TODO: Add images
+ */
+const HomeBottom = () => {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container></Container>
+    </Container>
+  );
+};
+
+export default HomeBottom;
